ca71d74e1734c9244863092b0a41cc07bf60c6ff
Nicolas Lopez <nl903828@gmail.com>
2022-08-27 16:09:59 +0700

    UI signup

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import DataRequestRoutes from './DataRequestRoutes';
 import Dashboard from '@/pages/dashboard';
 import SignIn from '@/pages/sign-in';
+import SignUp from '@/pages/sign-up';
 import Reports from '@/pages/reports';
 import Summary from '@/pages/summary';
 import TopBar from '@/components/main-layout/TopBar';
@@ -13,6 +14,7 @@ function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
         <Route
           path="dashboard"
           element={
