516e54047c5928f2cbe84384b76cc6789928e140
Nicolas Lopez <nl903828@gmail.com>
2022-08-31 00:18:46 +0700

    fix text error singup page

--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -77,13 +77,16 @@ const useStyles = makeStyles((theme) => ({
 
 const schema = yup
   .object({
-    email: yup.string().email().required(),
-    userName: yup.string().required(),
-    password: yup.string().required(),
+    email: yup
+      .string()
+      .email('Email must be a valid email')
+      .required('Email is a required field'),
+    userName: yup.string().required('User Name is a required field'),
+    password: yup.string().required('Password is a required field'),
     repeatPassword: yup
       .string()
-      .oneOf([yup.ref('password')], 'Passwords does not match')
-      .required(),
+      .oneOf([yup.ref('password')], 'Password does not match')
+      .required('Repeat Password is a required field'),
   })
   .required();
 
