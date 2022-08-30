ba3b502f44dedcb94da49befe1afbbf74a94dbcf
Nicolas Lopez <nl903828@gmail.com>
2022-08-31 00:22:05 +0700

    fix text error singup page

--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -81,7 +81,7 @@ const schema = yup
       .string()
       .email('Email must be a valid email')
       .required('Email is a required field'),
-    userName: yup.string().required('User Name is a required field'),
+    userName: yup.string().required('User name is a required field'),
     password: yup.string().required('Password is a required field'),
     repeatPassword: yup
       .string()
